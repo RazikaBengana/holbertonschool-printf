276140b13dc27907e872c2414fe1851b6bbcfe46
Razika-Bengana <5003@holbertonschool.com>
2022-07-08 13:25:41 +0200

    add print_c doc

--- print_c.c
+++ print_c.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * print_c - prints the current argument of a va_list, a char
+ * @args: list of arguments
+ *
+ * Return: 1
+*/
+
 int print_c(va_list args)
 {
 	char c = va_arg(args, int);
