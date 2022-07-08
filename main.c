0ec434a49aaf3d6481d0388a97d2cb709d85b57a
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-07-08 09:16:29 +0200

    Remove some tests and placeholders

--- main.c
+++ main.c
@@ -11,12 +11,16 @@ int main(void)
 	char *testing = "TEST";
 	int testagain = 654;
 
-	int s = _printf("test1: %s, test2: %s, test3: %dok", testing, testing, testagain);
+	int s = _printf("test1: %s, test2: %ss, test3: %dok", testing, testing, testagain);
 
 	printf("\n------- Nombre de caractères : ------------\n");
 	printf("%d\n", s);
 
 	_printf("test_n'existe pas: %r\nmême à la fin%");
+
+	int k = _printf("Hello, I'm %s", "Jose\n");
+	printf("\n------ Nombre de caractères : ---------- \n");
+	printf("%d\n", k);
 
 	return (0);
 }
--- print_c.c
+++ print_c.c
@@ -2,7 +2,5 @@
 
 int print_c(va_list args)
 {
-	/* Simple placeholder à modifier */
-	_putchar(va_arg(args));
 	return (0);
 }
